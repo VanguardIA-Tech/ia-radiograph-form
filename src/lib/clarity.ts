2d9e705fd29a87339c32c0043ee2deeb6c7dc735
Victor Meireles <victoraamlima@gmail.com>
2025-10-30 20:30:45 -0300

    Updated src/lib/clarity.ts

--- src/lib/clarity.ts
+++ src/lib/clarity.ts
@@ -2,9 +2,11 @@ import Clarity from "@microsoft/clarity";
 
 export function initClarity() {
   // Só roda no cliente e em produção
-  if (typeof window === "undefined" || !import.meta.env.PROD) return;
+
   console.log(typeof window === "undefined" || !import.meta.env.PROD)
   console.log(!import.meta.env.PROD)
+  if (typeof window === "undefined" || !import.meta.env.PROD) return;
+
 
 
   const projectId = import.meta.env.VITE_CLARITY_ID as string | undefined;
